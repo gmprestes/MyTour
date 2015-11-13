83b3e889982f62655c0e94a2cd152f6d603c03a2
Guilherme M. P. Silva <gmprestes@gmail.com>
2015-11-13 16:57:31 -0200

    Initial Commit

---
+++ main.js
@@ -0,0 +1,45 @@
+var elements = {
+    top: $('#selector-top'),
+    left: $('#selector-left'),
+    right: $('#selector-right'),
+    bottom: $('#selector-bottom')
+};
+
+var $target;
+$(document).mousemove(function(event) {
+    if(event.target.id.indexOf('selector') !== -1 || event.target.tagName === 'BODY' || event.target.tagName === 'HTML') return;
+
+    $target = $(event.target);
+        targetOffset = $target[0].getBoundingClientRect(),
+        targetHeight = targetOffset.height,
+        targetWidth  = targetOffset.width;
+    //console.log($target);
+
+    elements.top.css({
+        left:  (targetOffset.left - 4),
+        top:   (targetOffset.top - 4),
+        width: (targetWidth + 5)
+    });
+    elements.bottom.css({
+        top:   (targetOffset.top + targetHeight + 1),
+        left:  (targetOffset.left  - 3),
+        width: (targetWidth + 4)
+    });
+    elements.left.css({
+        left:   (targetOffset.left  - 5),
+        top:    (targetOffset.top  - 4),
+        height: (targetHeight + 8)
+    });
+    elements.right.css({
+        left:   (targetOffset.left + targetWidth + 1),
+        top:    (targetOffset.top  - 4),
+        height: (targetHeight + 8)
+    });
+
+});
+
+window.addEventListener('contextmenu', function(ev) {
+    ev.preventDefault();
+    console.log($target);
+    return false;
+}, false);
